fa1f2e06594bd8c45a3bd1c62c33c936e66bbeb3
witaliy-D <witaliy999@gmail.com>
2020-11-08 01:26:34 +0200

    del API_KEY

--- src/js/pageEntry/form.js
+++ src/js/pageEntry/form.js
@@ -1,9 +1,9 @@
 /* eslint-env jquery*/
 
+import 'core-js/modules/es.object.assign';
 import 'webp-in-css/polyfill';
 import svg4everybody from 'svg4everybody';
 svg4everybody();
-import 'core-js/modules/es.object.assign';
 
 import '../../blocks/burger/burger';
 import '../../blocks/field-text/field-text';
--- src/js/pageEntry/index.js
+++ src/js/pageEntry/index.js
@@ -1,10 +1,11 @@
 /* eslint-env jquery*/
 
+import 'core-js/modules/es.number.is-nan';
 import 'webp-in-css/polyfill';
 import svg4everybody from 'svg4everybody';
 svg4everybody();
 import 'picturefill';
-import 'core-js/modules/es.number.is-nan';
+
 
 import '../../blocks/burger/burger';
 import '../../blocks/review/review';
